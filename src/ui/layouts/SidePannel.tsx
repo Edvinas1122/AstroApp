3c52c07d59e1b7c36065c8272de58cce84312ddd
Edvinas Momkus <35561374+Edvinas1122@users.noreply.github.com>
2025-07-13 16:54:10 +0000

    pannel controlls

--- src/ui/layouts/SidePannel.tsx
+++ src/ui/layouts/SidePannel.tsx
@@ -15,34 +15,58 @@ export function SidePannelLayout({
 	class: className = '',
 }: SidePannelLayoutProps) {
 
-	const [media, setMedia] = useState(false);
+	const [mediaLeft, setMediaLeft] = useState(false);
+	const [mediaRight, setMediaRight] = useState(false);
 
 	const scaleHandle = (ev: MediaQueryListEvent) => {
 		console.log('scale trigger :)', ev.matches);
-		setMedia(ev.matches);
+		setMediaLeft(ev.matches);
+		setMediaRight(ev.matches);
 	}
 
 	useEffect(() => {
-		const ob = window.matchMedia("(width > 720px)");
-		setMedia(ob.matches);
+		const ob = window.matchMedia("(width > 820px)");
+		setMediaLeft(ob.matches); setMediaRight(ob.matches);
 		ob.addEventListener('change', scaleHandle);
 		return () => ob.removeEventListener('change', scaleHandle);
 	}, [])
+
+	const toggleLeft = () => setMediaLeft(!mediaLeft)
+	const toggleRight = () => setMediaRight(!mediaRight)
+
+	const renderButton = (symbol: string, action: () => void) => <button
+		class={style.cButton}
+		onClick={action}
+	>
+		{symbol}
+	</button>
 
 	return (
 		<div class={`${style.layout} ${className}`}>
 			<aside class={style.panel} 
 				style={{
-					width: media ? "24%" : "30px"
+					maxWidth: "200px",
+					width: mediaLeft ? "20%" : "30px"
 				}}
 			>
 				{left}
+				<div class={style.sideCtrl}>
+				{renderButton(mediaLeft ? "<" : ">", toggleLeft)}
+				</div>
             </aside>
             <main class={style.center}>
                 {children}
             </main>
-            <aside class={style.panel}>
+            <aside class={style.panel}
+				style={{
+					maxWidth: "250px",
+					width: mediaRight ? "20%" : "30px"
+				}}
+			>
                 {right}
+				<div class={style.sideCtrl}>
+				{renderButton(mediaRight ? ">" : "<", toggleRight)}
+				</div>
             </aside>
         </div>
     );
